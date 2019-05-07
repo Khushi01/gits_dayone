56e85aa5671f7b661e5f95cda37af288f41fd303
neha <jeelsharma1999@gmail.com>
2019-05-07 10:09:58 +0530

    First Upload

---
+++ app/src/main/java/com/example/hp/hello/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hp.hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import static android.R.attr.tag;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("TAG","hello");
+
+    }
+}
